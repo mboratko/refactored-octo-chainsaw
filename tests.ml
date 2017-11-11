1f5154e62c915c8481d2880f33d4b4301f40a64f
bkushigian <bkushigian@gmail.com>
2017-11-10 22:37:17 -0500

    Updated tests.ml

--- tests.ml
+++ tests.ml
@@ -1,9 +1,20 @@
+(* tests.ml
+ * NOTE: Tests should not be written here directly; this simply imports tests
+ * and collects/runs them. To include tests, write them in `some_test_file.ml`
+ * and add `open Some_test_file` to the top of this file
+ *)
+
 open Verif
 open Smtlib
 open Helpers
 open Imp
-open Ben_wp
-module PTest = Ppx_test.Test
 
+(*  vvv  Add your test files HERE!  vvv  *)
+open Ben_wp
+
+
+
+(*  vvv  No touching!  vvv  *)
+module PTest = Ppx_test.Test
 
 let _ = Ppx_test.Test.collect ()
